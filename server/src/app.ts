9693afaf34e2596769d83cec226efaacb4da20e1
Ubuntu <ubuntu@ip-172-31-15-98.sa-east-1.compute.internal>
2022-06-19 04:45:40 +0000

    Server: Fix: Helmet CSP

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import xss from 'xss-clean';
 import cors from 'cors';
 import rateLimiter from 'express-rate-limit';
+import path from 'path';
 import pool from './db';
 import activitiesRouter from './routes/activities';
 import authenticationRouter from './routes/auth';
@@ -17,26 +18,33 @@ const app = express();
 
 app.set('trust proxy', 1);
 app.use(rateLimiter({
-	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 100, // limit each IP to 100 requests per windowMs
+		windowMs: 5 * 60 * 1000, // 5 minutes
+			max: 100, // limit each IP to 100 requests per windowMs
 }));
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({
+	contentSecurityPolicy: {
+		directives: {
+			'default-src': ["'self'", "'unsafe-inline'", 'https:']
+		},
+	},
+}));
 app.use(cors());
 app.use(xss());
 
+app.use('/', express.static(path.join(__dirname, '../../client/build/')));
 app.use('/api/v1/auth', authenticationRouter);
 app.use('/api/v1/activities', authenticationMiddleware, activitiesRouter);
 
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 (async () => {
-	try {
-		await pool.getConnection();
-		console.log('DB connected');
-		app.listen(port, () => console.log(`Server listening on port ${port}`));
-	} catch (error) {
-		console.error(error);
-	}
+		try {
+					await pool.getConnection();
+							console.log('DB connected');
+									app.listen(port, () => console.log(`Server listening on port ${port}`));
+										} catch (error) {
+													console.error(error);
+														}
 })();
